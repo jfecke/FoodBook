e06c3a000535fb11d079ab043a6a901d066012bd
rachelsue <wells.rachelsue@gmail.com>
2019-03-22 21:21:53 -0500

    initial client and dependencies

--- .history/client/src/reducers/index_20190322205330.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer.js';
-import errorReducer from './errorReducer.js';
-
-export default combineReducers({
-  auth: authReducer,
-  errors: errorReducer
-});
