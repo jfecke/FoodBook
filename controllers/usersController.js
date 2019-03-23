d9bc699a2bb604c03d1aef2cf7fb2ef2f916e2c1
DillonPittman <pittmanwdillon@gmail.com>
2019-03-23 12:16:07 -0500

    changed state key to stateName

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,116 +10,119 @@ const validateLoginInput = require("../validation/login");
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function(req, res) {
-    db.User
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    const {errors, isValid} = validateRegisterInput(req.body);
+	findAll: function(req, res) {
+		db.User.find(req.query)
+			.sort({ date: -1 })
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	create: function(req, res) {
+		const { errors, isValid } = validateRegisterInput(req.body);
 
-    //Check Validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+		//Check Validation
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
 
-    db.User.findOne({email: req.body.email})
-      .then(user => {
-        if (user) {
-          errors.email = "Email already exists";
-          return res.status(400).json(errors);
-        } else {
-          console.log(req.body);
-          //Create object of new user using info received from front end
-          const newUser = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            city: req.body.city,
-            state: req.body.state,
-            profilepic: req.body.profilepic
-          }
-          
-          bcrypt.genSalt(10, (error, salt) => {
-            bcrypt.hash(newUser.password, salt, (error, hash) => {
-              if (error) throw error;
-              newUser.password = hash;
-              console.log(newUser);
-              db.User.create(newUser)
-                .then(user => {
-                  res.json({
-                    name: user.name,
-                    email: user.email,
-                    profilepic: user.profilepic
-                  });
-                })
-                .catch(error => console.log(error));
-            })
-          })
-        }
-      }
-    )
-  },
-  login: function(req, res) {
-    const {errors, isValid} = validateLoginInput(req.body);
+		db.User.findOne({ email: req.body.email }).then(user => {
+			if (user) {
+				errors.email = "Email already exists";
+				return res.status(400).json(errors);
+			} else {
+				console.log(req.body);
+				//Create object of new user using info received from front end
+				const newUser = {
+					name: req.body.name,
+					email: req.body.email,
+					password: req.body.password,
+					city: req.body.city,
+					state: req.body.stateName,
+					profilepic: req.body.profilepic,
+				};
 
-    //Check Validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
-    const email = req.body.email;
-    const password = req.body.password;
+				bcrypt.genSalt(10, (error, salt) => {
+					bcrypt.hash(newUser.password, salt, (error, hash) => {
+						if (error) throw error;
+						newUser.password = hash;
+						console.log(newUser);
+						db.User.create(newUser)
+							.then(user => {
+								res.json({
+									name: user.name,
+									email: user.email,
+									profilepic: user.profilepic,
+								});
+							})
+							.catch(error => console.log(error));
+					});
+				});
+			}
+		});
+	},
+	login: function(req, res) {
+		const { errors, isValid } = validateLoginInput(req.body);
 
-    //Find User by Email
-    db.User.findOne({email: email})
-      .then(user => {
-        if (!user) {
-          errors.email = "User not found";
-          return res.status(404).json(errors);
-        }
+		//Check Validation
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
+		const email = req.body.email;
+		const password = req.body.password;
 
-        //Check password
-        bcrypt.compare(password, user.password)
-          .then(isMatch => {
-            if (isMatch) {
-              //User Matched
+		//Find User by Email
+		db.User.findOne({ email: email })
+			.then(user => {
+				if (!user) {
+					errors.email = "User not found";
+					return res.status(404).json(errors);
+				}
 
-              //Create JWT Payload
-              const payload = {id: user.id, name: user.name, profilepic: user.profilepic}
+				//Check password
+				bcrypt.compare(password, user.password).then(isMatch => {
+					if (isMatch) {
+						//User Matched
 
-              //Sign Token
-              jwt.sign(payload, keys.secrets.secretOrKey, {expiresIn: 3600}, (error, token) => {
-                res.json({
-                  success: true,
-                  token:  "Bearer " + token
-                })
-              });
-            } else {
-              errors.password = "Incorrect Password";
-              return res.status(400).json(errors);
-            }
-          })
-      })
-      .catch(errpr => res.status(422).json(error));
-  },
-  findById: function(req, res) {
-    db.User.findById(req.params.id)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.User
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.User
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+						//Create JWT Payload
+						const payload = {
+							id: user.id,
+							name: user.name,
+							profilepic: user.profilepic,
+						};
+
+						//Sign Token
+						jwt.sign(
+							payload,
+							keys.secrets.secretOrKey,
+							{ expiresIn: 3600 },
+							(error, token) => {
+								res.json({
+									success: true,
+									token: "Bearer " + token,
+								});
+							}
+						);
+					} else {
+						errors.password = "Incorrect Password";
+						return res.status(400).json(errors);
+					}
+				});
+			})
+			.catch(errpr => res.status(422).json(error));
+	},
+	findById: function(req, res) {
+		db.User.findById(req.params.id)
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	update: function(req, res) {
+		db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	remove: function(req, res) {
+		db.User.findById({ _id: req.params.id })
+			.then(dbModel => dbModel.remove())
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
 };
--- validation/register.js
+++ validation/register.js
@@ -1,66 +1,64 @@
 const Validator = require("validator");
-const isEmpty =  require("./is-empty.js");
+const isEmpty = require("./is-empty.js");
 
 module.exports = function validateRegisterInput(data) {
-    let errors = {};
+	let errors = {};
 
-    data.name = !isEmpty(data.name) ? data.name : "";
-    data.email = !isEmpty(data.email) ? data.email : "";
-    data.password = !isEmpty(data.password) ? data.password : "";
-    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+	data.name = !isEmpty(data.name) ? data.name : "";
+	data.email = !isEmpty(data.email) ? data.email : "";
+	data.password = !isEmpty(data.password) ? data.password : "";
+	data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-    if (!Validator.isLength(data.name, {min: 2, max: 30})) {
-        errors.name = "Name must be between 2 and 30 characters";
-    }
+	if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+		errors.name = "Name must be between 2 and 30 characters";
+	}
 
-    if (Validator.isEmpty(data.name)) {
-        errors.name = "Name field is required";
-    }
+	if (Validator.isEmpty(data.name)) {
+		errors.name = "Name field is required";
+	}
 
-    if (Validator.isEmpty(data.email)) {
-        errors.email = "Email field is required";
-    }
+	if (Validator.isEmpty(data.email)) {
+		errors.email = "Email field is required";
+	}
 
-    if (!Validator.isEmail(data.email)) {
-        errors.email = "Email is invalid";
-    }
+	if (!Validator.isEmail(data.email)) {
+		errors.email = "Email is invalid";
+	}
 
-    if (Validator.isEmpty(data.password)) {
-        errors.password = "Password field is required";
-    }
-    
-    if (!Validator.isLength(data.password, {min: 6, max: 30})) {
-        errors.password = "Password must be at least 6 characters";
-    }
+	if (Validator.isEmpty(data.password)) {
+		errors.password = "Password field is required";
+	}
 
-    if (Validator.isEmpty(data.password2)) {
-        errors.password2 = "Comfirm Password field is required";
-    }
-    
-    if (!Validator.equals(data.password, data.password2)) {
-        errors.password2 = "Passwords must match";
-    }
+	if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+		errors.password = "Password must be at least 6 characters";
+	}
 
-    if (Validator.isEmpty(data.city)) {
-        errors.city = "City field is required";
-    }
-    
-    if (!Validator.isLength(data.city, {min: 2, max: 30})) {
-        errors.city = "Password must be at least 2 characters";
-    }
+	if (Validator.isEmpty(data.password2)) {
+		errors.password2 = "Comfirm Password field is required";
+	}
 
-    if (Validator.isEmpty(data.state)) {
-        errors.state = "State field is required";
-    }
-    
-    if (!Validator.isLength(data.state, {min: 2, max: 2})) {
-        errors.state = "State must be 2 characters";
-    }
+	if (!Validator.equals(data.password, data.password2)) {
+		errors.password2 = "Passwords must match";
+	}
 
+	if (Validator.isEmpty(data.city)) {
+		errors.city = "City field is required";
+	}
 
+	if (!Validator.isLength(data.city, { min: 2, max: 30 })) {
+		errors.city = "Password must be at least 2 characters";
+	}
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
+	if (Validator.isEmpty(data.stateName)) {
+		errors.state = "State field is required";
+	}
+
+	if (!Validator.isLength(data.stateName, { min: 2, max: 2 })) {
+		errors.state = "State must be 2 characters";
+	}
+
+	return {
+		errors,
+		isValid: isEmpty(errors),
+	};
+};
