74a0ce387a387a62ea0fcdb6b39daed690ef02a8
DillonPittman <pittmanwdillon@gmail.com>
2019-03-23 23:18:02 -0500

    dashboard added

---
+++ client/src/components/review/FollowReview.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Review extends Component {
+	render() {
+		return <div />;
+	}
+}
---
+++ client/src/components/review/UserReview.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class UserReview extends Component {
+	render() {
+		return <div className="card">hello there</div>;
+	}
+}
+
+// import UserReview from "../dashboard/UserReview";
+
+// userReview = <div>Also Loading...</div>;
+
+// userReview = <UserReview />;
+
+// let userReview;
+
+{
+	/* <div className="container">
+<div className="row">
+  <div className="col-md-12">{userReview}</div>
+</div>
+</div> */
+}
