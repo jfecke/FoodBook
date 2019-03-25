1580a36660f9c4d7ab05fafde2aca1b88d8365f5
DillonPittman <pittmanwdillon@gmail.com>
2019-03-24 22:31:36 -0500

    UserReview and FollowReview cards

--- client/src/components/review/FollowReview.js
+++ client/src/components/review/FollowReview.js
@@ -2,6 +2,26 @@ import React, { Component } from "react";
 
 export default class Review extends Component {
 	render() {
-		return <div />;
+		return (
+			<div className="card">
+				<div className="row">
+					{/* <div className="col-md"> */}
+					<img
+						className="restaruant-pic"
+						src="https://picsum.photos/160"
+						alt="profile"
+					/>
+					{/* </div> */}
+					<div className="col-md">
+						<h3>Restaurant Name</h3>
+						<div>Address</div>
+						<div># of Reviews by Follows</div>
+						<div>Yelp Rating</div>
+						<div>Foodbook Rating</div>
+						<a href="javascript:alert('Expand!');"># of Comments</a>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
--- client/src/components/review/UserReview.js
+++ client/src/components/review/UserReview.js
@@ -2,22 +2,24 @@ import React, { Component } from "react";
 
 export default class UserReview extends Component {
 	render() {
-		return <div className="card">hello there</div>;
+		return (
+			<div className="card">
+				<div className="row">
+					<img
+						className="restaruant-pic"
+						src="https://picsum.photos/160"
+						alt="profile"
+					/>
+					<div className="col-md">
+						<h3>Restaurant Name</h3>
+						<div>Address</div>
+						<div># of Reviews by Follows</div>
+						<div>Yelp Rating</div>
+						<div>Foodbook Rating</div>
+						<a href="javascript:alert('Expand!');"># of Comments</a>
+					</div>
+				</div>
+			</div>
+		);
 	}
-}
-
-// import UserReview from "../dashboard/UserReview";
-
-// userReview = <div>Also Loading...</div>;
-
-// userReview = <UserReview />;
-
-// let userReview;
-
-{
-	/* <div className="container">
-<div className="row">
-  <div className="col-md-12">{userReview}</div>
-</div>
-</div> */
 }
