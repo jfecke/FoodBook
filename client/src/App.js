8bf3b1e95a60560e5899b73234df30de1abadb67
rachelsue <wells.rachelsue@gmail.com>
2019-03-26 19:31:22 -0500

    redirect routes to login page if user tries to access private pages without being logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
@@ -17,65 +17,97 @@ import CommentCard from "./components/comments/CommentCard";
 import UserReview from "./components/review/UserReview";
 import CommentForm from "./components/comments/CommentForm";
 import { clearCurrentProfile } from "./actions/profileActions";
+import PrivateRoute from "./components/common/PrivateRoute";
+// import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 
 // Check for token
 if (localStorage.jwtToken) {
-	// Set auth token header auth
-	setAuthToken(localStorage.jwtToken);
-	// Decode token, get User info and expiration
-	const decoded = jwt_decode(localStorage.jwtToken);
-	// Set User and isAuthenticated
-	store.dispatch(setCurrentUser(decoded));
-	// Check for expired token
-	const currentTime = Date.now() / 1000;
-	if (decoded.exp < currentTime) {
-		// Logout User
-		store.dispatch(logoutUser());
-		// Clear current profile
-		store.dispatch(clearCurrentProfile());
-		// Redirect to Login
-		window.location.href = "/login";
-	}
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token, get User info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set User and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout User
+    store.dispatch(logoutUser());
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to Login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<Router>
-					<div className="App">
-						<Navbar />
-						<Route exact path="/" component={Landing} />
-						<div className="container">
-							<Route exact path="/register" component={Register} />
-							<Route exact path="/login" component={Login} />
-							{/* <Route path="/dashboard" component={Dashboard} /> */}
-							<Route path="/dashboard/review" component={FollowReview} />
-							<Route
-								exact
-								path="/dashboard/review/comment"
-								component={CommentCard}
-							/>
-							<Route path="/dashboard/userreview" component={UserReview} />
-							<Route
-								exact
-								path="/dashboard/userreview/comment"
-								component={CommentCard}
-							/>
-							<Route
-								exact
-								path="/comment/commentform"
-								component={CommentForm}
-							/>
-						</div>
-						<Footer />
-					</div>
-				</Router>
-			</Provider>
-		);
-	}
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+
+              {/* <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch> */}
+              {/* <Route path="/dashboard" component={Dashboard} /> */}
+
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/dashboard/review"
+                  component={FollowReview}
+                />
+              </Switch>
+
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/dashboard/review/comment"
+                  component={CommentCard}
+                />
+              </Switch>
+
+              <Switch>
+                <PrivateRoute
+                  path="/dashboard/userreview"
+                  component={UserReview}
+                />
+              </Switch>
+
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/dashboard/userreview/comment"
+                  component={CommentCard}
+                />
+              </Switch>
+
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/comment/commentform"
+                  component={CommentForm}
+                />
+              </Switch>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
