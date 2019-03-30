a29049e8afdf212a8e01624327c2ba55001a7322
rachelsue <wells.rachelsue@gmail.com>
2019-03-30 12:23:07 -0500

    reviewBtn

--- client/src/components/cards/RestaurantCard.js
+++ client/src/components/cards/RestaurantCard.js
@@ -3,65 +3,69 @@ import "../search/styles.css";
 import ph from "../../img/ph.png";
 
 const RestaurantCard = props => {
-	const star = <i className="fa fa-star" aria-hidden="true" />;
-	const halfstar = <i className="fas fa-star-half-alt" />;
+  const star = <i className="fa fa-star" aria-hidden="true" />;
+  const halfstar = <i className="fas fa-star-half-alt" />;
 
-	return (
-		<div className="card d-flex flex-row">
-			<div className="img-wrap d-flex justify-content-center">
-				<img id={props.id} alt={props.id} src={ph} />
-			</div>
-			<div
-				className="main-content d-flex flex-column justify-content-between"
-				style={{ flex: 1, marginLeft: 5 }}
-			>
-				<div className="d-flex flex-row row1">
-					<div style={{ flex: 1 }}>
-						<p className="title">Chueys</p>
-					</div>
+  return (
+    <div className="card d-flex flex-row">
+      <div className="img-wrap d-flex justify-content-center">
+        <img id={props.id} alt={props.id} src={ph} />
+      </div>
+      <div
+        className="main-content d-flex flex-column justify-content-between"
+        style={{ flex: 1, marginLeft: 5 }}
+      >
+        <div className="d-flex flex-row row1">
+          <div style={{ flex: 1 }}>
+            <p className="title">Chueys</p>
+          </div>
 
-					<div
-						className="d-flex flex-row justify-content-md-end muted"
-						style={{ flex: 1 }}
-					>
-						<p>0.5 miles</p>
-					</div>
-				</div>
+          <div
+            className="d-flex flex-row justify-content-md-end muted"
+            style={{ flex: 1 }}
+          >
+            <p>0.5 miles</p>
+          </div>
+        </div>
 
-				<div className="d-flex flex-row" style={{ flex: 1 }}>
-					<div className="d-flex flex-row row2" style={{ flex: 1 }}>
-						{star}
-						{star}
-						{halfstar}
+        <div className="d-flex flex-row" style={{ flex: 1 }}>
+          <div className="d-flex flex-row row2" style={{ flex: 1 }}>
+            {star}
+            {star}
+            {halfstar}
 
-						<p style={{ marginLeft: 5 }}>59 reviews</p>
-						<div
-							className="d-flex flex-row justify-content-end muted"
-							style={{ flex: 1 }}
-						>
-							<p>$$$$</p>
-						</div>
-					</div>
-				</div>
+            <p style={{ marginLeft: 5 }}>59 reviews</p>
+            <div
+              className="d-flex flex-row justify-content-end muted"
+              style={{ flex: 1 }}
+            >
+              <p>$$$$</p>
+            </div>
+          </div>
+        </div>
 
-				<p>Italian</p>
+        <p>Italian</p>
 
-				<div className="d-flex flex-row" style={{ flex: 1}}>
-			
-				<div className="d-flex flex-row" style={{ flex:1}}>
-				
+        <div className="d-flex flex-row" style={{ flex: 1 }}>
+          <div className="d-flex flex-row" style={{ flex: 1 }}>
+            <p className="muted" style={{ width: 100 + "%" }}>
+              209 E 58 St, Eastside
+            </p>
 
-				<p className="muted" style={{width:100+'%'}}>209 E 58 St, Eastside</p>
+            <a
+              href="/search/review"
+              className="btn btn-dark justify-content-end"
+              style={{ float: "right", margin: 1 }}
+            >
+              Review
+            </a>
+          </div>
+        </div>
+      </div>
 
-				<a href="/search/review" className="btn btn-dark justify-content-end" style={{float:"right", margin:1}}>Review</a>
-			</div>
-			</div>
-			</div>
-			
-			
-			<p />
-		</div>
-	);
+      <p />
+    </div>
+  );
 };
 
 export default RestaurantCard;
--- client/src/components/comments/CommentCard.js
+++ client/src/components/comments/CommentCard.js
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
 
 export default class Comment extends Component {
-	render() {
-		return (
-			<div className="card">
-				<div className="row">
-					<div className="col-md-2">
-						<img
-							className="comment-pic"
-							src="https://picsum.photos/160"
-							alt="profile"
-						/>
-						<div className="text-center">User Name</div>
-					</div>
-					<div className="col-md">
-						<div>Review Title</div>
-						<div>
-							<div>Rating ★★★★</div>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-							enim ad minim veniam, quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore eu fugiat
-							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-							sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="card">
+        <div className="row">
+          <div className="col-md-2">
+            <img
+              className="comment-pic"
+              src="https://picsum.photos/160"
+              alt="profile"
+            />
+            <div className="text-center">User Name</div>
+          </div>
+          <div className="col-md">
+            <div>Review Title</div>
+            <div>
+              <div>Rating ★★★★</div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
