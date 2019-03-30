fdfeadb4f0e5395bf5b2dbd941b26f665a7b94b8
DillonPittman <pittmanwdillon@gmail.com>
2019-03-29 19:16:39 -0500

    profile cards

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import ProfileCard from "../cards/ProfileCard";
+import { FormBtn, Input } from "./Search";
+import "./styles.css";
+
+// localhost:3000/search
+
+class Restaurants extends Component {
+	render() {
+		return (
+			<div>
+				<form className="d-flex flex-column" style={{ width: 100 + "%" }}>
+					<h1>Foodies</h1>
+					<div className="d-flex flex-row" style={{ flex: 1 }}>
+						<Input
+							className="d-flex flex-row"
+							style={{ flex: 1, width: 100 + "%" }}
+							// value={this.state.query}
+							// onChange={this.handleInputChange}
+							name="query"
+							placeholder="Search for users..."
+						/>
+						<FormBtn
+							className="d-flex flex-row justify-content-end search-btn"
+							style={{ flex: 1, float: "right", padding: 5 }}
+							// disabled={!this.state.query}
+							// onClick={this.handleFormSubmit}
+						/>
+					</div>
+				</form>
+
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+				<ProfileCard />
+			</div>
+		);
+	}
+}
+
+export default Restaurants;
