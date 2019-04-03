fbbb29f8ed027efb4e7338f8fc4186a1aa607205
jfecke <jfecke@gmail.com>
2019-04-02 21:36:43 -0500

    made user search work on foodie page

--- client/src/components/cards/ProfileCard.js
+++ client/src/components/cards/ProfileCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../search/styles.css";
-import ph from "../../img/ph.png";
+//import ph from "../../img/ph.png";
 
 const ProfileCard = props => {
   return (
     <div className="card d-flex flex-row">
       <div className="img-wrap d-flex justify-content-center">
-        <img id={props.id} alt={props.id} src={ph} />
+        <img id={props.id} alt={props.id} src={props.imageurl} />
       </div>
       <div
         className="main-content d-flex flex-column justify-content-between"
@@ -14,21 +14,21 @@ const ProfileCard = props => {
       >
         <div className="d-flex flex-row row1">
           <div style={{ flex: 1 }}>
-            <p className="title">UserName</p>
+            <p className="title">{props.name}</p>
           </div>
 
           <div
             className="d-flex flex-row justify-content-md-end muted"
             style={{ flex: 1 }}
           >
-            <button className="btn btn-primary">Follow</button>
+            <button className="btn btn-primary" id={props.id} onClick={props.follow}>Follow</button>
           </div>
         </div>
 
-        <p>Number of Followers</p>
-        <p>Number of Reviews</p>
+        <p>Number of Followers: {props.id}</p>
+        <p>Number of Reviews:  {props.reviews}</p>
 
-        <p className="muted">Location</p>
+        <p className="muted">Location: {props.location}</p>
       </div>
       <p />
     </div>
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,11 +1,84 @@
 import React, { Component } from "react";
 import ProfileCard from "../cards/ProfileCard";
 import { FormBtn, Input } from "./Search";
+import API from "../../utils/API"
 import "./styles.css";
 
 // localhost:3000/search
 
 class Restaurants extends Component {
+  constructor () {
+    super();
+    this.state = {
+      query: "",
+      users: []
+    };
+    this.findReviews = this.findReviews.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.findFollowers = this.findFollowers.bind(this);
+  }
+
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleClick = (event) => {
+    event.preventDefault();
+    API.getUsers({name: {$regex: "/"+this.state.query+"/" }}).then(results => {
+      this.findFollowers(results.data);
+    })
+  };
+
+  findFollowers = (results) => {
+    let userdata = results.map((user) => {
+      return new Promise(function(res) { 
+          API.getfollowers({
+            FollowingId: user._id
+          }).then(function(resultsOBJ) {
+            let numfollowers = 0;
+            for (let i in resultsOBJ.data) {
+              numfollowers++
+            }
+            user["followers"] = numfollowers;
+            res( user )
+          });
+      })
+  })
+    Promise.all(userdata).then(alldata => {
+      //console.log(this.state.query);
+      this.findReviews(alldata);
+    })
+  }
+
+  findReviews = (results) =>  {
+    let userdata = results.map((user) => {
+      return new Promise(function(res) { 
+          API.getReviews({
+            UserId: user._id
+          }).then(function(resultsOBJ) {
+            console.log(resultsOBJ.data)
+            let numReviews = resultsOBJ.data.length;
+            user["reviews"] = numReviews;
+            res(user)
+          });
+      })
+  })
+    Promise.all(userdata).then(alldata => {
+      this.setState({users: alldata});
+    })
+  }
+
+  handleFollow = event => {
+    event.preventDefault();
+    console.log(event.target)
+  }
+
+ 
+
   render() {
     return (
       <div>
@@ -15,8 +88,8 @@ class Restaurants extends Component {
             <Input
               className="d-flex flex-row"
               style={{ flex: 1, width: 100 + "%" }}
-              // value={this.state.query}
-              // onChange={this.handleInputChange}
+              value={this.state.query}
+              onChange={this.handleInputChange}
               name="query"
               placeholder="Search for users..."
             />
@@ -24,21 +97,23 @@ class Restaurants extends Component {
               className="d-flex flex-row justify-content-end search-btn"
               style={{ flex: 1, float: "right", padding: 5 }}
               // disabled={!this.state.query}
-              // onClick={this.handleFormSubmit}
+              onClick={this.handleClick}
             />
           </div>
         </form>
+           {this.state.users.map(user => (
+                <ProfileCard
+                  id={user._id}
+                  imageurl={user.profilePic}
+                  name={user.name}
+                  key={user._id}
+                  followers={user.followers}
+                  reviews={user.reviews}
+                  follow={this.handleFollow}
+                  location={user.city + ", "+user.stateName}>
 
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+                </ProfileCard>
+              ))}
       </div>
     );
   }
