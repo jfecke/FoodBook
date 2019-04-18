ee8343ed5dae56722fc59eae1eba957b26972c5d
jfecke <jfecke@gmail.com>
2019-04-17 20:46:04 -0500

    added resend token functionality

--- controllers/confirmationController.js
+++ controllers/confirmationController.js
@@ -27,6 +27,47 @@ module.exports = {
             })
             .catch(err => res.status(422).json(err));
         })
-        .catch(err => res.status(422).json(err));
+        .catch(error => res.status(422).json(error));
+    },
+    resend: function(req, res) {
+        const errors = {};
+         User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    errors.resend = "We were unable to find a user with that email.";
+                    return res.status(400).json(errors);
+                };
+                if (user.isVerified) {
+                    errors.resend = "This account has already been verified. Please log in.";
+                    return res.status(400).json(errors);
+                };
+                const token = {
+                    UserId: user._id,
+                    token: crypto.randomBytes(16).toString("hex")
+                };
+                db.Token.create(token).then((token) => {
+                    const transporter = nodemailer.createTransport({
+                        service: "Sendgrid",
+                            auth: {
+                            user: keys.secrets.sendgridusername,
+                            pass: keys.secrets.sendgridpass
+                        }
+                    });
+                    var mailOptions = {
+                        from: "no-reply@foodbook223.com",
+                        to: user.email,
+                        subject: "FoodBook Account Verification",
+                        text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/api\/confirmation\/' + token.token + '.\n'
+                    };
+                        transporter.sendMail(mailOptions, error => {
+                        if (error) { 
+                            return res.json(error); 
+                        };
+                        res.status(200).send("A verification email has been sent to: " + user.email + ".");
+                    });
+                })
+                .catch(error => console.log(error));
+            })
+            .catch(error => res.status(422).json(error));
     }
 }
--- routes/api/confirmation.js
+++ routes/api/confirmation.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const confirmationController = require("../../controllers/confirmationController");
 
+router.route("/")
+  .post(confirmationController.resend)
+
 router.route("/:token")
   .get(confirmationController.confirm)
 
